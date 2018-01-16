dd34a6111b2a156c8a11c2e739de57c8314a390c
Viktor <viktor.palm.vp@gmail.com>
2018-01-16 06:33:29 +0100

    working

--- rainbow/color_control.cpp
+++ rainbow/color_control.cpp
@@ -1,1 +1,26 @@
-int* shiftColor(int* color);
+#include "color_control.h"
+#include <stdlib.h> 
+
+void shiftColor(double* color, double* goalColor, double shiftSpeed){
+  double redShift, greenShift, blueShift, totalColor;
+  
+  totalColor = abs(color[0] - goalColor[0]) + abs(color[1] - goalColor[1]) + abs(color[2] - goalColor[2]);
+
+  redShift = (color[0] - goalColor[0])/ totalColor;
+  greenShift = (color[1] - goalColor[1])/ totalColor;
+  blueShift = (color[2] - goalColor[2])/ totalColor;
+
+
+  
+  //if ((redShift*shiftSpeed > 0 && color[0] + redShift*shiftSpeed < goalColor[0]) || (redShift*shiftSpeed < 0 && color[0] + redShift*shiftSpeed > goalColor[0]))
+    color[0] = color[0] + (redShift*shiftSpeed);
+    color[1] = color[1] + (greenShift*shiftSpeed);
+    color[2] = color[2] + (blueShift*shiftSpeed);
+  //}
+  /*else{
+    color[0] = goalColor[0];
+    color[1] = goalColor[1];
+    color[2] = goalColor[2];
+  }*/
+  
+}
